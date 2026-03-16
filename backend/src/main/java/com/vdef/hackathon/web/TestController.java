2f1be381d8e110131c7e9c427c7932a38e73ab9c
Clearwater <neoware@laposte.net>
2026-03-16 11:35:28 +0100

    :tada: Init Spring Boot backend, backend compose.yaml and backend Dockerfile.

---
+++ backend/src/main/java/com/vdef/hackathon/web/TestController.java
@@ -0,0 +1,14 @@
+package com.vdef.hackathon.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController
+{
+    @GetMapping("/")
+    public String index()
+    {
+        return "test";
+    }
+}
