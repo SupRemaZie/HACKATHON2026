ee9b6f749715a9778b5ca945b21465b908463443
Clearwater <neoware@laposte.net>
2026-03-17 11:31:26 +0100

    :wrench: :sparkles: Added POST routing to create sites ( /sites/create ). Renamed some variables to be coherent with schema.

--- backend/src/main/java/com/vdef/hackathon/controller/SiteController.java
+++ backend/src/main/java/com/vdef/hackathon/controller/SiteController.java
@@ -1,10 +1,9 @@
 package com.vdef.hackathon.controller;
 
+import com.vdef.hackathon.dto.site.CreateSiteRequest;
 import com.vdef.hackathon.jpa.SiteJPA;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.vdef.hackathon.repository.SiteRepository;
+import org.springframework.web.bind.annotation.*;
 import com.vdef.hackathon.service.*;
 
 import java.util.List;
@@ -15,9 +14,27 @@ import java.util.UUID;
 public class SiteController {
 
     private final ServiceSite siteService;
+    private final SiteRepository siteRepository;
 
-    public SiteController(ServiceSite siteService) {
+    public SiteController(ServiceSite siteService, SiteRepository siteRepository) {
         this.siteService = siteService;
+        this.siteRepository = siteRepository;
+    }
+
+    @PostMapping("/site/create")
+    public String createSite(@RequestBody CreateSiteRequest request)
+    {
+        SiteJPA site = new SiteJPA();
+        site.setName(request.siteName());
+        site.setSurfaceM2(request.areaM2().doubleValue());
+        site.setNbEmployees(request.employees());
+        site.setNbWorkstations(request.nbWorkstations());
+        site.setParkingUnderground(request.parkingSpots());
+        site.setParkingOutdoor(request.parkingSpots());
+        site.setParkingBasement(request.parkingSpots());
+        site.setCreatedBy(UUID.fromString("00000000-0000-0000-0000-000000000001"));
+        siteRepository.save(site);
+        return "ok";
     }
 
     @GetMapping("/site/{uuid}")
